747a2a932bc798eba2d89c019677401cbcd415ce
awdse22 <145016508+awdse22@users.noreply.github.com>
2024-06-02 15:30:20 +0800

    Revert "Merge branch 'outdoormap'"
    This reverts commit 163ba0b7f97b9018b73860674e72287dc3a30415, reversing
    changes made to 609a06dbe79449b89e52424c97790ec8780b5c9c.

--- screens/OutdoorMap.js
+++ screens/OutdoorMap.js
@@ -1,17 +1,13 @@
 import { useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, Image, TouchableWithoutFeedback, Keyboard, FlatList } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { createRoot } from "react-dom/client";
+import { APIProvider, Map, MapCameraChangedEvent } from '@vis.gl/react-google-maps';
 import axios from 'axios';
 
 export default function OutdoorMap() {
   const navigation = useNavigation();
-  const [region, setRegion] = useState({
-    latitude: 40.749933,
-    longitude: -73.98633,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+  const [mapCenter, setMapCenter] = useState({ lat: 40.749933, lng: -73.98633 });
   const [searchSuggestions, setSearchSuggestions] = useState([]);
   const [searchInput, setSearchInput] = useState('');
 
@@ -24,11 +20,7 @@ export default function OutdoorMap() {
       const { results } = response.data;
       if (results.length > 0) {
         const { lat, lng } = results[0].geometry.location;
-        setRegion({
-          ...region,
-          latitude: lat,
-          longitude: lng,
-        });
+        setMapCenter({ lat, lng });
       } else {
         console.log('No results found');
       }
@@ -58,53 +50,72 @@ export default function OutdoorMap() {
     }
   }, [searchInput]);
 
-  const SearchBar = () => (
-    <View style={styles.searchBar.container}>
-      <TextInput
-        onChangeText={setSearchInput}
-        value={searchInput}
-        style={styles.searchBar.input}
-        placeholder='Search'
-      />
-      <Button title='Search' onPress={() => handleSearch(searchInput)} />
-      {searchSuggestions.length > 0 && (
-        <FlatList
-          data={searchSuggestions}
-          keyExtractor={(item) => item.place_id}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => {
-              setSearchInput(item.description);
-              handleSearch(item.description);
-              setSearchSuggestions([]);
-            }}>
-              <Text style={styles.searchBar.suggestion}>{item.description}</Text>
-            </TouchableOpacity>
-          )}
+  const SearchBar = () => {
+    return (
+      <View style={styles.searchBar.container}>
+        <TextInput
+          onChangeText={setSearchInput}
+          value={searchInput}
+          style={styles.searchBar.input}
+          placeholder='Search'
         />
-      )}
-    </View>
-  );
+        <Button title='Search' onPress={() => handleSearch(searchInput)} />
+        {searchSuggestions.length > 0 && (
+          <FlatList
+            data={searchSuggestions}
+            keyExtractor={(item) => item.place_id}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => {
+                setSearchInput(item.description);
+                handleSearch(item.description);
+                setSearchSuggestions([]);
+              }}>
+                <Text style={styles.searchBar.suggestion}>{item.description}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        )}
+      </View>
+    );
+  };
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.mainUIContainer}>
         <SearchBar />
-        <MapView
-          style={{ alignSelf: 'stretch', height: '80%' }}
-          region={region}
-          onRegionChangeComplete={newRegion => setRegion(newRegion)}
-        >
-          <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
-        </MapView>
+        <APIProvider apiKey={'AIzaSyBYYtYwdIsgeOtEKmVA1wdKe1DI98Q8-z4'} onLoad={() => console.log('Maps API has loaded.')}>
+          <Map
+            defaultZoom={13}
+            center={mapCenter}
+            onCameraChanged={(event) => {
+              const newCenter = event.detail.center;
+              setMapCenter({ lat: newCenter.lat, lng: newCenter.lng });
+            }}>
+          </Map>
+        </APIProvider>
       </View>
     </TouchableWithoutFeedback>
   );
 }
 
+
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: 0,
+    flexDirection: 'column',
+    flex: 1,
+    borderWidth: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  // Outdoor Map
   mainUIContainer: {
     paddingTop: 20,
+    flexDirection: 'column',
     flex: 1,
+    borderWidth: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
   },
@@ -118,6 +129,7 @@ const styles = StyleSheet.create({
       backgroundColor: '#ededed',
     },
     input: {
+      flex: 1,
       height: 50,
       fontSize: 25,
     },
