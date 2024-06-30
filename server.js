5d677e9ef615f5a0e2da8686997cbaabd58269e6
awdse22 <145016508+awdse22@users.noreply.github.com>
2024-06-30 17:42:49 +0800

    Deploying to Heroku

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.use(
   userPagesRouter,
 );
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
